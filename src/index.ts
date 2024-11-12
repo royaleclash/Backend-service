6b4e4e817ceb54fbd2984fa196e0335f1331f221
royaleclash <sincabeza90@gmail.com>
2024-11-12 08:20:22 -0500

    area de preparacion

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Application } from "express";
+import cors from "cors";
+import { AppDataSource } from "./data-source";
+import productRoutes from "./routes/HamburgerRoutes";
+import swaggerUI from "swagger-ui-express";
+import swaggerSpec from "./swagger/swagger";
+import HamburgerRoutes from "./routes/HamburgerRoutes";
+
+const app: Application = express();
+const PORT = process.env.PORT ?? 3000;
+
+// Middleware - Guardianes de ruta
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+app.use("/api/hamburger", HamburgerRoutes);
+
+// Documentación Swagger
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+// Inicialización de la base de datos y el servidor
+AppDataSource.initialize()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}\n`);
+
+      console.log(`Endpoints:`);
+      console.log(`API Products http://localhost:${PORT}/api/products`);
+
+      console.log(`Documentación:`);
+      console.log(`Swagger en http://localhost:${PORT}/api-docs`);
+    });
+  })
+  .catch((error) => console.log(error));
